85ca71a6b3bf5caade2bfbbee500d9bb8d01c0bb
PSURI1894 <parthsuri009@gmail.com>
2026-05-17 17:20:00 +0530

    test: add unit test profiles for temporal dynamic cdc joins, iteration 31

--- flink-jobs/src/main/java/com/iot/streaming/model/RawEvent.java
+++ flink-jobs/src/main/java/com/iot/streaming/model/RawEvent.java
@@ -34,4 +34,4 @@ public class RawEvent implements Serializable {
     public Integer getQualityFlag() { return qualityFlag; }
     public void setQualityFlag(Integer qualityFlag) { this.qualityFlag = qualityFlag; }
 }
-// Version update 30\n
+// Version update 31\n
